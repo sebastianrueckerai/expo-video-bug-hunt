cd44dd4a70b8e2299215c4e3de89c9c23a8b5b74
unknown <Ruecker.Sebastian@googlemail.com>
2022-06-18 17:03:46 +0200

    added more urls

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import Constants from "expo-constants";
+import { Video, ResizeMode } from "expo-av";
+
+const sourceUrls = [
+  "https://tantra-app-bucket.s3.eu-central-1.amazonaws.com/15936669715030.0_Intro.mp4",
+  "https://d3w2cdkkkvjjfr.cloudfront.net/1601392561415_Zen_Loss_2_Fear_of_loss.mp4",
+  "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8",
+  "https://media.graphcms.com/jyLnBuPTOaiOgZxP0fR3",
+  "https://multiplatform-f.akamaihd.net/i/multi/april11/sintel/sintel-hd_,512x288_450_b,640x360_700_b,768x432_1000_b,1024x576_1400_m,.mp4.csmil/master.m3u8",
+  "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+  "http://media.developer.dolby.com/Atmos/MP4/Universe_Fury2.mp4",
+  "http://media.developer.dolby.com/Atmos/MP4/shattered-3Mb.mp4",
+  "http://media.developer.dolby.com/DDP/MP4_HPL40_30fps_channel_id_51.mp4",
+  "http://media.developer.dolby.com/DDP/MP4_HPL40_30fps_channel_id_71.mp4",
+];
+
+// or any pure javascript modules available in npm
+//import { Card } from 'react-native-paper';
 
 export default function App() {
+  const [urlIdx, seturlIdx] = useState(0);
+  const uri = sourceUrls[urlIdx];
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Video
+        usePoster={true}
+        source={{ uri }}
+        rate={1.0}
+        volume={1.0}
+        isMuted={false}
+        resizeMode={ResizeMode.CONTAIN}
+        shouldPlay={true}
+        isLooping={false}
+        useNativeControls={true}
+        style={{
+          width: "100%",
+          flex: 1,
+          backgroundColor: "black",
+          position: "relative",
+        }}
+      />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          height: 100,
+          padding: 20,
+        }}
+      >
+        <Button
+          disabled={urlIdx === 0}
+          onPress={() => seturlIdx(urlIdx - 1)}
+          title="<- Prev"
+          color="#841584"
+        />
+        <Button
+          disabled={urlIdx === sourceUrls.length - 1}
+          onPress={() => seturlIdx(urlIdx + 1)}
+          title="Next ->"
+          color="#841584"
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +69,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
   },
 });
